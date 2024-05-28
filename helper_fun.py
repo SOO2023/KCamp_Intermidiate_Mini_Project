c497fad204ea57c0636964d91a52c92c8f3f994b
Samson Olaide <xamxynolaide10@gmail.com>
2024-05-28 13:18:33 +0100

    second commit

--- helper_fun.py
+++ helper_fun.py
@@ -1,3 +1,7 @@
 def id_gen (dict_obj):
-    last_id = list(dict_obj.keys())[-1]
-    return last_id + 1
+    keys = list(dict_obj.keys())
+    if len(keys) != 0:
+        last_id = list(dict_obj.keys())[-1]
+        return last_id + 1
+    else:
+        return 1
